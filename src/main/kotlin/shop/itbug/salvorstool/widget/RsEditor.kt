959ce2a82217bd3f2e4ddb311d63a036f81eb663
梁典典 <413153189@qq.com>
2024-04-25 14:00:04 +0800

    添加编辑器字体

--- src/main/kotlin/shop/itbug/salvorstool/widget/RsEditor.kt
+++ src/main/kotlin/shop/itbug/salvorstool/widget/RsEditor.kt
@@ -1,18 +1,33 @@
 package shop.itbug.salvorstool.widget
 
 import com.intellij.lang.Language
+import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.colors.EditorFontType
 import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.ui.EditorSettingsProvider
 import com.intellij.ui.LanguageTextField
+import com.intellij.util.ui.JBFont
 import org.rust.lang.RsLanguage
+import java.awt.Dimension
+import java.awt.Font
 import javax.swing.BorderFactory
+import javax.swing.border.Border
 
-class RsEditor(project: Project, initText: String) : LanguageTextField(Language.findInstance(RsLanguage::class.java),project,initText,false) {
+class RsEditor(projectP: Project, initText: String) : LanguageTextField(Language.findInstance(RsLanguage::class.java),projectP,initText,false) {
 
     override fun createEditor(): EditorEx {
         return myCreateEditor(super.createEditor())
     }
 
+    override fun getFont(): Font {
+        return EditorColorsManager.getInstance().globalScheme.getFont(EditorFontType.PLAIN)
+    }
+
+    override fun getBorder(): Border {
+        return BorderFactory.createEmptyBorder(0,0,0,0)
+    }
 }
  fun myCreateEditor(ex: EditorEx): EditorEx {
     ex.setVerticalScrollbarVisible(true)
